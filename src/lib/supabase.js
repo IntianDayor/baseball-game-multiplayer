b6a134a9c0e97127b9baf53e5b869665446ccdfc
IntianDayor <dayorferaer@gmail.com>
2026-06-02 11:54:52 +0800

    Todo list for supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,3 +4,10 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+/*
+    TODO:
+    ENABLE RLS ON ALL TABLES
+    WRITE RLS POLICIES
+    TEST SECURITY
+*/
