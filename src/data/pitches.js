22ed6fc6aa0b814e274302741fd59cf4eb3d6585
IntianDayor <dayorferaer@gmail.com>
2026-07-21 10:01:40 +0800

    Added more Pitch types

--- src/data/pitches.js
+++ src/data/pitches.js
@@ -1,71 +1,134 @@
 export const PITCH_LIBRARY = {
   fastball: {
     name: "Fastball",
-    key: "Q",
     speed: 10,
     breakX: 0,
     breakY: -1,
     breakTiming: 0,
-    color: "#ff4444",
     fixed: true,
   },
+
+  twoSeam: {
+    name: "Two-Seam Fastball",
+    speed: 9,
+    breakX: -2,
+    breakY: -2,
+    breakTiming: 0.4,
+  },
+
+  cutter: {
+    name: "Cutter",
+    speed: 9,
+    breakX: 2,
+    breakY: -1,
+    breakTiming: 0.7,
+  },
+
+  sinker: {
+    name: "Sinker",
+    speed: 8,
+    breakX: -1,
+    breakY: -4,
+    breakTiming: 0.6,
+  },
+
+  splitter: {
+    name: "Splitter",
+    speed: 7,
+    breakX: 0,
+    breakY: -6,
+    breakTiming: 0.85,
+  },
+
+  forkball: {
+    name: "Forkball",
+    speed: 6,
+    breakX: 0,
+    breakY: -6,
+    breakTiming: 0.8,
+  },
+
+  changeup: {
+    name: "Changeup",
+    speed: 4,
+    breakX: 1,
+    breakY: -2,
+    breakTiming: 0.3,
+    disguised: true,
+  },
+
+  circleChange: {
+    name: "Circle Change",
+    speed: 4,
+    breakX: -2,
+    breakY: -3,
+    breakTiming: 0.5,
+    disguised: true,
+  },
+
   curveball: {
     name: "Curveball",
     speed: 5,
     breakX: 1,
     breakY: -5,
     breakTiming: 0,
-    color: "#ffffff",
   },
+
+  knuckleCurve: {
+    name: "Knuckle Curve",
+    speed: 5,
+    breakX: 1,
+    breakY: -5,
+    breakTiming: 0.4,
+  },
+
   slider: {
     name: "Slider",
     speed: 7,
     breakX: 4,
     breakY: -2,
     breakTiming: 0.7,
-    color: "#00cfff",
   },
+
+  sweeper: {
+    name: "Sweeper",
+    speed: 6,
+    breakX: 6,
+    breakY: -1,
+    breakTiming: 0.75,
+  },
+
   slurve: {
     name: "Slurve",
     speed: 6,
     breakX: 3,
     breakY: -3,
-    breakTiming: 0,
-    color: "#a78bfa",
+    breakTiming: 0.5,
   },
+
+  screwball: {
+    name: "Screwball",
+    speed: 6,
+    breakX: -4,
+    breakY: -3,
+    breakTiming: 0.5,
+  },
+
   knuckleball: {
     name: "Knuckleball",
     speed: 3,
     breakX: "random",
     breakY: "random",
     breakTiming: 0,
-    color: "#fbbf24",
     chaos: true,
   },
-  screwball: {
-    name: "Screwball",
-    speed: 6,
-    breakX: -4,
-    breakY: -3,
-    breakTiming: 0,
-    color: "#f97316",
-  },
-  changeup: {
-    name: "Changeup",
-    speed: 4,
+
+  eephus: {
+    name: "Eephus",
+    speed: 2,
     breakX: 0,
-    breakY: -2,
+    breakY: -1,
     breakTiming: 0,
-    color: "#84cc16",
-    disguised: true,
-  },
-  forkball: {
-    name: "Forkball",
-    speed: 6,
-    breakX: 0,
-    breakY: -5,
-    breakTiming: 0.8,
-    color: "#ec4899",
   },
 }
 
